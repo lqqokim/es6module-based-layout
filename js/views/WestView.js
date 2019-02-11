6999f2419752ef3e346103456dcd3c524f40284c
kiminsu <kis420404@gmail.com>
2019-02-12 03:13:30 +0900

    Box Model, Position 관련 레이아웃 수정 & 트렌지션 추가

--- js/views/WestView.js
+++ js/views/WestView.js
@@ -6,6 +6,10 @@ WestView.setup = function (el) {
     this.init(el);
     this.slideBtnEl = el.querySelector('.west .slide-btn');
     this.expandBtnEls = el.querySelectorAll('.west .expand-btn');
+    this.contentItemEls = el.querySelectorAll('.west .content-item');
+
+    this.mainEl = document.querySelector('.main');
+
     this.bindEvents();
     return this;
 }
@@ -19,13 +23,13 @@ WestView.bindEvents = function () {
     //slide 버튼 클릭 이벤트등록
     this.slideBtnEl.addEventListener('click', e => {
         this.el.classList.remove('opened');
-        this.el.classList.add('closed');
+        this.mainEl.classList.add('lose-left');
     });
     
     //expand 버튼 클릭 이벤트등록
     this.expandBtnEls.forEach((expandBtn, index) => {
         expandBtn.addEventListener('click', e => {
-            this.onExpand(expandBtn, index);
+            this.contentItemEls[index].classList.add('expand');
         })
     });
 }
@@ -35,7 +39,8 @@ WestView.onClickTab = function (tabIndex) {
 }
 
 WestView.onExpand = function (el, index) {
-    console.log(el, index);
+
+    // console.log(el, index);
 }
 
 export default WestView;
